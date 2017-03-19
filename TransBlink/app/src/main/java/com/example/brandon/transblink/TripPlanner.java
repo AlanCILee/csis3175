2b81a978ed609ee283776cc0c12037b92bdcfade
Andrew T <andrewt25@outlook.com>
2017-03-19 13:50:22 -0700

    added station names to autocomplete text fields

--- TransBlink/app/src/main/java/com/example/brandon/transblink/TripPlanner.java
+++ TransBlink/app/src/main/java/com/example/brandon/transblink/TripPlanner.java
@@ -4,14 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TripPlanner extends AppCompatActivity {
     Station startStation;
+    Station station;
+    List<String> stationDisp;
     ArrayList<Station> stations;
     private int themeSel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +29,40 @@ public class TripPlanner extends AppCompatActivity {
         setContentView(R.layout.activity_trip_planner);
 
         String caller = (String)intent.getSerializableExtra("caller");
+        stations = (ArrayList<Station>) intent.getSerializableExtra("stations");
+        stationDisp = new ArrayList<>();
+
+        for(int i = 0; i < stations.size(); i++)
+        {
+            station = stations.get(i);
+            stationDisp.add(station.getFullName());
+        }
+
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, stationDisp);
+
+
+        AutoCompleteTextView start = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextViewStartingStation);
+        AutoCompleteTextView stop = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextViewEndStation);
+
+        start.setAdapter(adapter);
+        stop.setAdapter(adapter);
+
+
+
 
         switch (caller){
             case "LocationStation":
                 startStation = (Station)intent.getSerializableExtra("startStation");
-                stations = (ArrayList<Station>) intent.getSerializableExtra("stations");
                 Log.d("startStation",startStation.getFullName());
+                start.setText(startStation.getFullName());
+
+
                 break;
 
             case "MainActivity":
-                stations = (ArrayList<Station>) intent.getSerializableExtra("stations");
+
                 break;
 
         }
