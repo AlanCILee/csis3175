ad3c3a140e7af166551bbd37caa391d8881dc668
BRANDON-PC\Brandon <brandon.n.f@hotmail.ca>
2017-03-30 16:19:40 -0700

    theme is on every part of it, route display screen is set up.

--- TransBlink/app/src/main/java/com/example/brandon/transblink/RouteDisplay.java
+++ TransBlink/app/src/main/java/com/example/brandon/transblink/RouteDisplay.java
@@ -4,10 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RouteDisplay extends AppCompatActivity {
+    public int themeSel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        ThemeChange themeChg = new ThemeChange();
+        themeSel = themeChg.findTheme(this);
+        setTheme(themeSel);
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_display);
+    }
+
+    public void drawRouteOnMap(){
+
+
+
     }
 }
